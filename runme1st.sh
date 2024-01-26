adedaf002540ac95584896c6b17e4cb9ee0758e4
thebastien69 <36190193+thebastien69@users.noreply.github.com>
2024-01-26 11:52:24 +0100

    Add files via upload

--- runme1st.sh
+++ runme1st.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
+
+### Prerequis installation ###
+
 sudo apt install gcc binutils make perl liblzma-dev mtools mkisofs syslinux git -yq
 # Package mkisofs is not available, but is referred to by another package.
-#This may mean that the package is missing, has been obsoleted, or
-#is only available from another source
-#However the following packages replace it:
+# This may mean that the package is missing, has been obsoleted, or
+# is only available from another source
+# However the following packages replace it:
 #  genisoimage
 sudo apt install gcc binutils make perl liblzma-dev mtools genisoimage syslinux git -yq
 
+### Getting ipxe drivers from github ###
+script_dir=$(realpath $(dirname $0))
+echo
+echo ### git clone ipxe in $script_dir/ipxe ###
+cd $script_dir/../
+echo "Cloning ipxe"
 git clone https://github.com/ipxe/ipxe.git
 
-cp -vf boot.ipxe ./ipxe/src/
-
+### Compilation ###
+echo
+echo "BE SURE TO MODIFY $script_dir/boot.ipxe TO YOUR NEEDS BEFORE COMPILING !!!!!!"
+read -p "  Hit ENTER or CTRL-C ..." dummy
+cp -vf $script_dir/boot.ipxe $script_dir/../ipxe/src/
 cd ipxe/src
 make -j4 bin/ipxe.lkrn bin-x86_64-efi/ipxe.efi EMBED=boot.ipxe
 make -j4 bin/undionly.kpxe EMBED=boot.ipxe
-
-### Copie de fichier d  plac  e au lancement du container dans /rundrbl.sh
-#RUN cp bin-x86_64-efi/ipxe.efi /srv/tftp/ipxe.efi
-#RUN cp bin/undionly.kpxe /srv/tftp/undionly.kpxe
-##############################################################
-
